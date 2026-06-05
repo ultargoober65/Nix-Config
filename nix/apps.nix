47b3da4f33ef2ba1e3ef216ac426c1ad234f8b3f
ultragoober65 <matiasnyrhinen07@gmail.com>
2026-06-05 23:53:10 +0300

    meow

--- nix/apps.nix
+++ nix/apps.nix
@@ -5,6 +5,7 @@
  environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+  bottles
   pkgs.neovim
   pkgs.kitty
   pkgs.prismlauncher
