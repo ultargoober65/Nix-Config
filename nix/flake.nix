42bf575a308581fa6fa6d17644d024dfdf952304
ultragoober65 <matiasnyrhinen07@gmail.com>
2026-06-05 22:14:52 +0300

    meow

--- nix/flake.nix
+++ nix/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    hyprland.url = "github:hyprwm/hyprland?ref=v0.36.0";
+    hyprland.url = "github:hyprwm/hyprland";
 
      home-manager = {
        url = "github:nix-community/home-manager";
